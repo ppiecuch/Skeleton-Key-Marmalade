b50ceef60329168e9305fc0e138c29e3748d7378
ppiecuch <piecuch.pawel@gmail.com>
2013-10-04 09:43:39 +0200

    workarounds for wp8 building

---
+++ source/s3e_externals.c
@@ -0,0 +1,7 @@
+#if WINDOWS_PHONE && ( defined(__x86__) || defined(_M_I86) || defined(_M_IX86) )
+
+// dummy definitions for incomplete x86 build for Windows Phone 8
+
+void s3eDebugIsDebuggerPresent();
+
+#endif
