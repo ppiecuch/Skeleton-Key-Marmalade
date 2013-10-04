4274ebf7ec34c6aab73c7fe4b891a612767ba877
ppiecuch <piecuch.pawel@gmail.com>
2013-10-04 16:02:22 +0200

    more externals symbols

--- source/s3e_externals.c
+++ source/s3e_externals.c
@@ -8,6 +8,8 @@
 // dummy definitions for incomplete x86 build for Windows Phone 8;
 // since we build for mobile only, exclude only __arm__ builds.
 
+void DllEntryPoint() {}
+
 void s3eTimerGetMs() {}
 void s3eDebugIsDebuggerPresent() {}
 void s3eDebugOutputString() {}
