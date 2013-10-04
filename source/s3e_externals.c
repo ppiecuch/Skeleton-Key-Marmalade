3e197cf7ffb565ab9f93ea2d2983d2cba210e409
ppiecuch <piecuch.pawel@gmail.com>
2013-10-04 14:35:32 +0200

    more externals symbols

--- source/s3e_externals.c
+++ source/s3e_externals.c
@@ -1,5 +1,9 @@
 
-#if !defined(__arm__)
+#if defined(__arm__) || defined(arm)
+
+// skip - arm build is fine
+
+#else
 
 // dummy definitions for incomplete x86 build for Windows Phone 8;
 // since we build for mobile only, exclude only __arm__ builds.
