f8d57b9de5002823feaf10051b9605af58884720
ppiecuch <piecuch.pawel@gmail.com>
2013-10-10 22:14:40 +0200

    wp8 build

--- source/s3e_externals.c
+++ source/s3e_externals.c
@@ -8,7 +8,7 @@
 // dummy definitions for incomplete x86 build for Windows Phone 8;
 // since we build for mobile only, exclude only __arm__ builds.
 
-void DllEntryPoint() {}
+void DllEntryPoint(int a1, int a2, int a3) {}
 
 void s3eTimerGetMs() {}
 void s3eDebugIsDebuggerPresent() {}
