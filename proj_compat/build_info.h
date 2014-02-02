e90b5330c3395cc5efb2c94770b81e45daea0d9a
ppiecuch <piecuch.pawel@gmail.com>
2014-02-03 00:01:35 +0100

    cleanup build script

--- proj_compat/build_info.h
+++ proj_compat/build_info.h
@@ -5,6 +5,6 @@
 #define SK_MINOR 1
 #define SK_REVISION 0
 #define SK_VERSION "1.1.0"
-#define SK_BUILDVERSION "1.1.0.3747"
-#define SK_BUILDTIME "14/02/2014 23:45"
+#define SK_BUILDVERSION "1.1.0.1403"
+#define SK_BUILDTIME "14/02/2014 00:01"
 #define SK_SOURCE_REV 0xaa9e2ff
